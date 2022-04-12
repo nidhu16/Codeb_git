15ee1a852229fabe0e8587176aff8dbea5635851
nidhu16 <srinidhi.murali@praxis.ac.in>
2022-04-12 22:09:55 +0530

    fries

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ hungry = input("Are you hungry?")
 
 if hungry == "yes":
     print("Have your food")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
 else:
-    print("Do your work")
+    thirsty = input("Are you thirsty?")
+    if thirsty == "yes":
+        print("Drink water")
+        print("Drink soda")
